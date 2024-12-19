c4b4af9a4ccb874bf23baebcbe217d34249fc136
MauricioJrB <mjunior1205@gmail.com>
2024-12-18 22:37:32 -0300

    feat: connect project with mongodb

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,16 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
+
+import { connectDB } from './config/database';
 
 dotenv.config();
 
 const app = express();
+app.use(express.json());
+connectDB();
 
 app.get('/', (req: Request, res: Response) => {
-  res.status(200).json({ message: "Hello World" });
+  res.status(200).json({ message: 'Hello World' });
 });
 
 export default app;
