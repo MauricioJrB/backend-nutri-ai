43d105a9cdc7f8af97114a8f375a8151dd0109a7
MauricioJrB <mjunior1205@gmail.com>
2025-03-07 17:30:31 -0300

    rename(IUserData): renamed IUser to IUserData interface

--- src/Interfaces/IUserData.ts
+++ src/Interfaces/IUserData.ts
@@ -1,5 +1,7 @@
-export default interface IUser {
-  id: string;
+import mongoose from 'mongoose';
+
+export default interface IUserData {
+  userId: mongoose.Types.ObjectId;
   username: string;
   age: number;
   gender: 'Feminino' | 'Masculino';
