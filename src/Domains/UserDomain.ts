4680932bfec17f5ea7dfa2ef095f6f72f9aee131
MauricioJrB <mjunior1205@gmail.com>
2025-03-09 16:53:49 -0300

    refactor(UserDomain): changed UserDomain to received name, email and password

--- src/Domains/UserDomain.ts
+++ src/Domains/UserDomain.ts
@@ -1,29 +1,16 @@
-import IUser from '../Interfaces/IUser';
+import mongoose from 'mongoose';
+import { IUser } from '../Interfaces/User';
 
 export default class UserDomain implements IUser {
-  public id: string;
-  public username: string;
-  public age: number;
-  public gender: 'Feminino' | 'Masculino';
-  public height: number;
-  public weight: number;
-  public level:
-    | 'Sedentario'
-    | 'Levemente ativo'
-    | 'Moderadamente ativo'
-    | 'Muito ativo';
-  public objective: 'Perder peso' | 'Ganhar musculo' | 'Manutencao';
-  public trainingFrequency: number;
+  public id: mongoose.Types.ObjectId;
+  public name: string;
+  public email: string;
+  public password: string;
 
   constructor(user: IUser) {
     this.id = user.id;
-    this.username = user.username;
-    this.age = user.age;
-    this.gender = user.gender;
-    this.height = user.height;
-    this.weight = user.weight;
-    this.level = user.level;
-    this.objective = user.objective;
-    this.trainingFrequency = user.trainingFrequency;
+    this.name = user.name;
+    this.email = user.email;
+    this.password = user.password;
   }
 }
