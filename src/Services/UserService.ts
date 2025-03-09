21c179ec1be3e3a4936afc413f8ed119436f2211
MauricioJrB <mjunior1205@gmail.com>
2025-03-09 16:55:38 -0300

    refactor(UserService): changed UseService to user business rule

--- src/Services/UserService.ts
+++ src/Services/UserService.ts
@@ -1,6 +1,6 @@
-import UserODM from '../Models/User';
-import IUser from '../Interfaces/IUser';
 import UserDomain from '../Domains/UserDomain';
+import { IUser } from '../Interfaces/User';
+import UserODM from '../Models/User';
 
 export default class UserService {
   private userODM = new UserODM();
@@ -10,15 +10,14 @@ export default class UserService {
   }
 
   public async create(user: IUser) {
-    const existingUser = await this.userODM.getUserByUsername(user.username);
-    if (existingUser) return false;
     const newUser = await this.userODM.create(user);
     return this.createUser(newUser);
   }
 
-  public async getAllUsers() {
-    const users = await this.userODM.getAllUsers();
-    return users.map((user) => this.createUser(user));
+  public async getUserByEmail(email: string) {
+    const user = await this.userODM.getUserByEmail(email);
+    if (!user) return null;
+    return this.createUser(user);
   }
 
   public async getUserById(id: string) {
@@ -27,11 +26,10 @@ export default class UserService {
     return this.createUser(user);
   }
 
-  public async updateUserById(id: string, user: IUser) {
-    const existingUser = await this.userODM.getUserById(id);
-    if (!existingUser) return false;
-    const updatedUser = await this.userODM.updateUserById(id, user);
-    return updatedUser;
+  public async updatePassword(id: string, password: string) {
+    const newPassword = await this.userODM.updatePassword(id, password);
+
+    return newPassword;
   }
 
   public async deleteUserById(id: string) {
