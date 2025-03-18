ae89420200e84e66b1b36e973b755712029dce6b
MauricioJrB <mjunior1205@gmail.com>
2025-03-18 15:07:11 -0300

    refactor(UserRepository): changed save method to return new user object

--- src/repositories/UserRepository.ts
+++ src/repositories/UserRepository.ts
@@ -1,6 +1,6 @@
+import { User } from '../entities/User';
 import { PrismaClient } from '@prisma/client';
-import User from '../entities/User';
-import { IUserRepository } from '../interfaces/IUser';
+import { IUserRepository } from '../interfaces/IUser/IUserRepository';
 
 export class UserRepository implements IUserRepository {
   private constructor(readonly prisma: PrismaClient) {}
@@ -9,41 +9,38 @@ export class UserRepository implements IUserRepository {
     return new UserRepository(prisma);
   }
 
-  public async save(user: User): Promise<void> {
-    try {
-      const data = {
-        name: user.name,
-        email: user.email,
-        password: user.password,
-      };
-      await this.prisma.user.create({ data });
-    } catch (error) {
-      console.error('Error saving user:', error);
-      throw new Error('Could not save user');
-    }
+  public async save(user: User): Promise<User> {
+    const data = {
+      name: user.name,
+      email: user.email,
+      password: user.password,
+    };
+
+    const savedUser = await this.prisma.user.create({ data });
+
+    return User.load(
+      savedUser.id,
+      savedUser.name,
+      savedUser.email,
+      savedUser.password,
+    );
   }
 
   public async find(id: string): Promise<User | null> {
     const userExists = await this.prisma.user.findUnique({ where: { id } });
-
     if (!userExists) return null;
 
     const { name, email, password } = userExists;
-
     const user = User.load(id, name, email, password);
-
     return user;
   }
 
-  async findByEmail(email: string): Promise<User | null> {
+  public async findByEmail(email: string): Promise<User | null> {
     const emailExists = await this.prisma.user.findUnique({ where: { email } });
-
     if (!emailExists) return null;
 
     const { id, name, password } = emailExists;
-
     const user = User.load(id, name, email, password);
-
     return user;
   }
 
