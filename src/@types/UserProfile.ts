4826068deae599d37a6d68226483d9b564ce3273
MauricioJrB <mjunior1205@gmail.com>
2025-05-23 19:20:13 -0300

    feat: added UserProfileProps

---
+++ src/@types/UserProfile.ts
@@ -0,0 +1,11 @@
+export type UserProfileProps = {
+  id?: string;
+  userId: string;
+  age: number;
+  gender: string;
+  height: number;
+  weight: number;
+  activityLevel: string;
+  objective: string;
+  activityFrequency: string;
+};
