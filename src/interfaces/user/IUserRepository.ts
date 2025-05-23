2126c515476eb50736327f29c5c9b24cbc5d1a77
MauricioJrB <mjunior1205@gmail.com>
2025-05-23 19:21:55 -0300

    fix: changed findByIdProvider in IUserRepository interface

--- src/interfaces/user/IUserRepository.ts
+++ src/interfaces/user/IUserRepository.ts
@@ -3,6 +3,7 @@ import { User } from '../../entities/User';
 export interface IUserRepository {
   save(user: User): Promise<User>;
   find(id: string): Promise<User | null>;
+  findByIdProvider(idProvider: string): Promise<User | null>;
   findByEmail(email: string): Promise<User | null>;
   update(id: string, password: string): Promise<void>;
   delete(id: string): Promise<void>;
