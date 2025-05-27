e844cd9a110a7bcb06686bef33f43159efcaeaff
MauricioJrB <mjunior1205@gmail.com>
2025-05-27 18:45:14 -0300

    refactor: updated IUserProfileRepository interface and removed find method

--- src/interfaces/userProfile/IUserProfileRepository.ts
+++ src/interfaces/userProfile/IUserProfileRepository.ts
@@ -1,10 +1,8 @@
-import { UserProfileProps } from '../../@types/UserProfile';
 import { UserProfile } from '../../entities/UserProfile';
 
 export interface IUserProfileRepository {
   save(data: UserProfile): Promise<UserProfile>;
-  find(id: string): Promise<UserProfile | null>;
   findByUserId(userId: string): Promise<UserProfile | null>;
-  update(data: Partial<UserProfileProps>): Promise<UserProfile>;
-  delete(id: string): Promise<void>;
+  update(id: string, data: UserProfile): Promise<UserProfile>;
+  delete(userId: string): Promise<void>;
 }
