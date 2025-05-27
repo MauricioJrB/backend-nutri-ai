07ad7c6ddcdd89b94cb7fe99c14f1f15015cbd7b
MauricioJrB <mjunior1205@gmail.com>
2025-05-27 19:03:50 -0300

    feat: added UserProfileRoutes

---
+++ src/api/express/routes/userProfile/UserProfileRoutes.ts
@@ -0,0 +1,65 @@
+import { Request, Response, Router } from 'express';
+import { UserProfileController } from '../../controllers/userProfile/UserProfileController';
+import { decodeToken } from '../../middlewares/auth/decodeToken';
+import { ensureAuthenticated } from '../../middlewares/auth/ensureAuthenticated';
+import { validateData } from '../../middlewares/validateData';
+import { userProfileSchemas } from '../../../../schemas/userProfileSchemas';
+
+export class UserProfileRoutes {
+  private router: Router;
+  private controller: UserProfileController;
+
+  constructor() {
+    this.router = Router();
+    this.controller = UserProfileController.build();
+    this.initializeRoutes();
+  }
+
+  public static build() {
+    return new UserProfileRoutes();
+  }
+
+  private initializeRoutes(): void {
+    this.router.post(
+      '/',
+      validateData(userProfileSchemas),
+      decodeToken,
+      ensureAuthenticated,
+      (req: Request, res: Response) => {
+        this.controller.save(req, res);
+      },
+    );
+
+    this.router.get(
+      '/me',
+      decodeToken,
+      ensureAuthenticated,
+      (req: Request, res: Response) => {
+        this.controller.findByUserId(req, res);
+      },
+    );
+
+    this.router.put(
+      '/me',
+      validateData(userProfileSchemas),
+      decodeToken,
+      ensureAuthenticated,
+      (req: Request, res: Response) => {
+        this.controller.update(req, res);
+      },
+    );
+
+    this.router.delete(
+      '/me',
+      decodeToken,
+      ensureAuthenticated,
+      (req: Request, res: Response) => {
+        this.controller.delete(req, res);
+      },
+    );
+  }
+
+  public getRouter(): Router {
+    return this.router;
+  }
+}
