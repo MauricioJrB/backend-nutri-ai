0617e24d23e7bb4b9bb1a9bbfd00e510c91cd2f2
MauricioJrB <mjunior1205@gmail.com>
2025-07-29 17:27:43 -0300

    refactor: update UserMapper to use object destructuring for User creation

--- src/mappers/UserMapper.ts
+++ src/mappers/UserMapper.ts
@@ -1,16 +1,18 @@
+import { UserProps } from '../@types/User';
 import { CreateUserDto, UserResponseDto } from '../dtos/UserDto';
 import { User } from '../entities/User';
 
 export class UserMapper {
   public static toEntity(dto: CreateUserDto): User {
-    return User.create(
-      dto.email,
-      dto.name,
-      dto.provider ?? 'email',
-      dto.idProvider ?? null,
-      dto.photoUrl ?? null,
-      dto.password ?? null,
-    );
+    return User.create({
+      id: dto.id || undefined,
+      email: dto.email,
+      name: dto.name,
+      provider: dto.provider || 'email',
+      idProvider: dto.idProvider ?? null,
+      photoUrl: dto.photoUrl ?? null,
+      password: dto.password ?? null,
+    });
   }
 
   public static toResponseDto(user: User): UserResponseDto {
@@ -33,5 +35,17 @@ export class UserMapper {
       photoUrl: user.photoUrl,
       token,
     };
+  }
+
+  public static fromPrisma(user: UserProps): User {
+    return User.create({
+      id: user.id || undefined,
+      email: user.email,
+      name: user.name,
+      provider: user.provider,
+      idProvider: user.idProvider,
+      photoUrl: user.photoUrl,
+      password: user.password,
+    });
   }
 }
